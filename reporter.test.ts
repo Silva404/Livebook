43bc78a30af1a8bcb344d294772e0dd17e8a0b0c
Breno <breno@Brenos-MBP.fritz.box>
2023-12-12 14:28:45 +0100

    Should merge correctly the new entries based on priority

--- reporter.test.ts
+++ reporter.test.ts
@@ -42,80 +42,6 @@ type TimeEntry = {
   end: string;
 };
 
-function newDateFromHour(hour: number): string {
-  const date = new Date();
-  date.setHours(hour);
-  return date.toISOString();
-}
-
-function convertLinearIssueToTimeEntry(entries: Array<LinearIssue>) {
-  let timeEntries: Array<TimeEntry> = [];
-
-  for (const entry of entries) {
-    const currentEntry = timeEntries.find(
-      (item) => item.id === entry.id && entry.type === "completed",
-    );
-
-    if (!currentEntry) {
-      timeEntries.push({
-        id: entry.id,
-        title: entry.title,
-        start: entry.createdAt,
-        end: entry.createdAt,
-      });
-      continue;
-    }
-
-    const rest = timeEntries.filter((item) => item.id !== entry.id);
-    timeEntries = [...rest, { ...currentEntry, end: entry.createdAt }];
-  }
-
-  return timeEntries;
-}
-
-test("should merge the same time entries with start and finish date", () => {
-  const linearIssues: Array<LinearIssue> = [
-    {
-      createdAt: newDateFromHour(9),
-      id: "5d25f17c-bef3-492c-8d38-156e58366261",
-      title: "TASK A",
-      type: "started",
-    },
-    {
-      createdAt: newDateFromHour(12),
-      id: "5d25f17c-bef3-492c-8d38-156e58366261",
-      title: "TASK A",
-      type: "completed",
-    },
-    {
-      createdAt: newDateFromHour(12),
-      id: "5d25f17c-bef3-492c-8d38-156e58366268",
-      title: "TASK B",
-      type: "completed",
-    },
-    {
-      createdAt: newDateFromHour(14),
-      id: "5d25f17c-bef3-492c-8d38-156e58366268",
-      title: "TASK B",
-      type: "completed",
-    },
-    {
-      createdAt: newDateFromHour(13),
-      id: "5d25f17c-bef3-492c-8d38-156e58366266",
-      title: "TASK C",
-      type: "completed",
-    },
-    {
-      createdAt: newDateFromHour(16),
-      id: "5d25f17c-bef3-492c-8d38-156e58366266",
-      title: "TASK C",
-      type: "completed",
-    },
-  ];
-  const entries = convertLinearIssueToTimeEntry(linearIssues);
-  // assert.deepEqual(entries, expected);
-});
-
 type TimeEntries = Array<TimeEntry>;
 
 function mergeTimeEntries(
@@ -161,6 +87,45 @@ function mergeTimeEntries(
   }
 
   return result;
+}
+
+function convertTimeEntriesToDateString(entries: TimeEntries) {
+  return entries.map((entry) => ({
+    ...entry,
+    start: new Date(entry.start).toDateString(),
+    end: new Date(entry.end).toDateString(),
+  }));
+}
+
+function newDateFromHour(hour: number): string {
+  const date = new Date();
+  date.setHours(hour);
+  return date.toISOString();
+}
+
+function convertLinearIssueToTimeEntry(entries: Array<LinearIssue>) {
+  let timeEntries: Array<TimeEntry> = [];
+
+  for (const entry of entries) {
+    const currentEntry = timeEntries.find(
+      (item) => item.id === entry.id && entry.type === "completed",
+    );
+
+    if (!currentEntry) {
+      timeEntries.push({
+        id: entry.id,
+        title: entry.title,
+        start: entry.createdAt,
+        end: entry.createdAt,
+      });
+      continue;
+    }
+
+    const rest = timeEntries.filter((item) => item.id !== entry.id);
+    timeEntries = [...rest, { ...currentEntry, end: entry.createdAt }];
+  }
+
+  return timeEntries;
 }
 
 test("should create a report start the time entries", () => {
@@ -221,45 +186,47 @@ test("should create a report start the time entries", () => {
     calendarEvents,
     convertLinearIssueToTimeEntry(linearIssues),
   );
-  console.log(result);
   const expected: TimeEntries = [
     {
       id: "5d25f17c-bef3-492c-8d38-156e58366261",
       title: "TASK A",
-      start: "2023-12-12T08:05:06.441Z",
-      end: "2023-12-12T09:05:06.441Z",
+      start: newDateFromHour(9),
+      end: newDateFromHour(10),
     },
     {
-      start: "2023-12-12T09:05:06.443Z",
-      end: "2023-12-12T10:05:06.443Z",
+      start: newDateFromHour(10),
+      end: newDateFromHour(11),
       id: "5d25f17c-bef3-492c-8d38-156e58366261",
       title: "Daily",
     },
     {
       id: "5d25f17c-bef3-492c-8d38-156e58366261",
       title: "TASK A",
-      start: "2023-12-12T10:05:06.441Z",
-      end: "2023-12-12T11:05:06.441Z",
+      start: newDateFromHour(11),
+      end: newDateFromHour(12),
     },
     {
       id: "5d25f17c-bef3-492c-8d38-156e58366268",
       title: "TASK B",
-      start: "2023-12-12T11:05:06.441Z",
-      end: "2023-12-12T12:05:06.441Z",
+      start: newDateFromHour(12),
+      end: newDateFromHour(13),
     },
     {
-      start: "2023-12-12T12:05:06.443Z",
-      end: "2023-12-12T14:05:06.443Z",
+      start: newDateFromHour(13),
+      end: newDateFromHour(15),
       id: "5d25f17c-bef3-492c-8d38-156e58366261",
       title: "Presentation",
     },
     {
       id: "5d25f17c-bef3-492c-8d38-156e58366266",
       title: "TASK C",
-      start: "2023-12-12T14:05:06.441Z",
-      end: "2023-12-12T15:05:06.441Z",
+      start: newDateFromHour(15),
+      end: newDateFromHour(16),
     },
   ];
-  // console.log(calendarEvents, linearIssues, expected);
-  // assert.deepEqual(result, expected);
+
+  assert.deepEqual(
+    convertTimeEntriesToDateString(result),
+    convertTimeEntriesToDateString(expected),
+  );
 });
